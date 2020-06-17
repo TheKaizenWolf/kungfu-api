c73510508792cfc2095078554aab1fe510209b06
TheKaizenWolf <raul670saavedra@gmail.com>
2020-06-17 06:45:00 -0400

    arguments

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
 const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  enum Status {
+    WATCHED
+    INTERESTED
+    NOT_INTERESTED
+    UNKNOWN
+  }
+
+  type Actor {
+    id: ID!
+    name: String!
+  }
+
+  type Movie {
+    id: ID!
+    title: String!
+    releaseDate: String
+    rating: Int
+    actor: [Actor]
+    status: Status
+  }
+
+  type Query {
+    movies: [Movie]
+    movie(id: ID): Movie
+  }
+`;
+
+const movies = [
+  {
+    id: 'sdfasdgioasasik',
+    title: '5 Deadly Venoms',
+    releaseDate: '10-10-1983',
+    rating: 5,
+  },
+  {
+    id: 'sgdasigsakgadios',
+    title: '36th Chamber',
+    releaseDate: '10-10-1983',
+    rating: 5,
+    actor: [
+      {
+        id: 'asdgdasgdas',
+        name: 'Gordon Liu',
+      },
+    ],
+  },
+];
+
+const resolvers = {
+  Query: {
+    movies: () => movies,
+    movie: (obj, { id }, context) => {
+      const foundMovie = movies.find(movie => movie.id === id);
+      return foundMovie;
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`Server started at ${url}`);
+});
